51b42cf33b0f0df017b8496ed42e35a146e12d7f
Ezzedine <ezzedineozone123@gmail.com>
2024-09-02 17:37:42 +0300

    sync commit

--- Sync Service/main.cpp
+++ Sync Service/main.cpp
@@ -10,14 +10,9 @@ int main()
 	std::cout << "Background Sync Service: 0.1.0\n" << "https://github.com/ezzedineozone/Sync-Service\n" <<
 		"this is a console app, can be fully controlled through commands\n" << "compatible UI for ease of use can be found at: https://github.com/ezzedineozone/ProjectSync\n"
 		<< "Ezzedine Al Ozon.\n";
-	//do {
-	//	std::cout << "sync_service 0.1.0 (? or help for details): ";
-	//} while (commands.request_command() != 0);
-	SyncModule test_module("module3", fs::path("C:\\test_folder"), fs::path("C:\\test_folder"), "local", "one-way");
-	nlohmann::json j = test_module.to_json();
-	std::cout << j << "\n";
-	SyncModule test_module_2(j);
-	std::cout << test_module_2.to_string() << "\n";
+	do {
+		std::cout << "sync_service 0.1.0 (? or help for details): ";
+	} while (commands.request_command() != 0);
 	std::cout << "program ended \n";
 };
 
